ed99482df1ba9020efe80789c1dc2315af45a410
Josh Reisner <josh@joshreisner.com>
2014-05-04 10:17:32 -0700

    reorganizing main file

---
+++ meetings.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Meetings
+ * Plugin URI: https://github.com/intergroup/plugin
+ * Description: CMS for maintaining database of meetings and locations
+ * Version: 1.0
+ * Author: Santa Clara County Intergroup
+ * Author URI: http://aasanjose.org
+ * License: none
+ */
+
+include('includes/variables.php');
+
+include('includes/functions.php');
+
+include('includes/init.php');
+
+include('includes/admin_edit.php');
+
+include('includes/save.php');
+
+//include('includes/admin_options.php');
+
+//include('includes/admin_lists.php');
