2d97e6fb697f7d556af7b66a7fa1f1de49a76f1d
Josh Reisner <josh@joshreisner.com>
2016-10-22 13:02:27 -0700

    2.6.9: upcoming meetings widget

---
+++ includes/widgets.php
@@ -0,0 +1,58 @@
+<?php
+
+class TSML_Widget_Upcoming extends WP_Widget {
+
+	//constructor
+	public function __construct() {
+		parent::__construct('tsml_widget_upcoming', __('Upcoming Meetings', '12-step-meeting-list'),
+			array(
+				'description' => __('Display a table of upcoming meetings.', '12-step-meeting-list'),
+			)
+		);
+	}
+
+	//front-end display of widget
+	public function widget($args, $instance) {
+		echo $args['before_widget'];
+		if (!empty($instance['title'])) {
+			echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+		}
+		echo tsml_next_meetings($instance);
+		echo '<p><a href="' . get_post_type_archive_link('tsml_meeting') . '">' . __('View All Meetings', '12-step-meeting-list') . '</a></p>';
+		echo $args['after_widget'];
+	}
+
+	//backend form
+	public function form($instance) {
+		$title = !empty($instance['title']) ? $instance['title'] : __('Upcoming Meetings', '12-step-meeting-list');
+		$count = !empty($instance['count']) ? $instance['count'] : 5;
+		?>
+		<p>
+			<label for="<?php echo esc_attr($this->get_field_id('title'))?>"><?php _e(esc_attr('Title:'))?></label> 
+			<input class="widefat" id="<?php echo esc_attr($this->get_field_id('title'))?>" name="<?php echo esc_attr($this->get_field_name('title'))?>" type="text" value="<?php echo esc_attr($title)?>">
+		</p>
+		<p>
+			<label for="<?php echo esc_attr($this->get_field_id('count'))?>"><?php _e(esc_attr('Count:'))?></label> 
+			<select id="<?php echo esc_attr($this->get_field_id('title'))?>" name="<?php echo esc_attr($this->get_field_name('count'))?>">
+				<?php for ($i = 1; $i < 11; $i++) {?>
+					<option value="<?php echo $i?>"<?php selected($i, esc_attr($count))?>><?php echo $i?></option>
+				<?php }?>
+			</select>
+		</p>
+		<?php 
+	}
+
+	//sanitize widget form values as they are saved
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = (! empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+		$instance['count'] = (! empty($new_instance['count'])) ? intval($new_instance['count']) : 5;
+		return $instance;
+	}
+}
+
+// register widget
+function tsml_widget() {
+    register_widget('TSML_Widget_Upcoming');
+}
+add_action('widgets_init', 'tsml_widget');
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: meetingguide, aasanjose
 Tags: meetings, aa, al-anon, na, 12-step, locations, groups
 Requires at least: 3.2
 Tested up to: 4.6
-Stable tag: 2.6.8
+Stable tag: 2.6.9
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -54,6 +54,7 @@ It was originally designed to maintain a list of AA meetings in Santa Clara Coun
 
 * [Lancaster, PA](http://lanclebalanon.org/meetings/?d=any)
 * [Orange County, CA](http://ocalanon-d60.org/meetings?d=any)
+* [Pennsylvania](http://pa-al-anon.org/meetings/)
 
 **CoDA**
 
@@ -179,6 +180,9 @@ and then drop some or all of these tags in your PHP:
 1. Edit location
 
 == Changelog ==
+
+= 2.6.9 =
+* Upcoming meetings widget
 
 = 2.6.8 =
 * Importer now catches empty rows earlier, no longer throws mysterious error about them
