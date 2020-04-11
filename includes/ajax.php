c6359291bf5cccea0bfaa5eda1cfe2441ea90c72
Mark Chouinard <mark@chouinard.me>
2020-04-10 18:16:16 -0700

    Formatting
    Signed-off-by: Mark Chouinard <mark@chouinard.me>

--- includes/ajax.php
+++ includes/ajax.php
@@ -659,15 +659,16 @@ add_action( 'wp_ajax_nopriv_meeting_link', 'tsml_ajax_meeting_link' );
  *
  */
 function tsml_ajax_meeting_link() {
+
 	global $tsml_nonce;
 
 	if ( ! isset( $_GET['meeting_id'] ) || ! isset( $_GET['nonce'] ) || ! wp_verify_nonce( $_GET['nonce'], $tsml_nonce ) ) {
 		wp_send_json_error();
 	}
 	$meeting_id = intval( $_GET['meeting_id'] );
-	$url = get_post_meta( $meeting_id, 'conference_url', true );
+	$url        = get_post_meta( $meeting_id, 'conference_url', true );
 	if ( $url ) {
-		wp_send_json_success( ['meeting' => $url]);
+		wp_send_json_success( [ 'meeting' => $url ] );
 	} else {
 		wp_send_json_error();
 	}
@@ -683,13 +684,14 @@ add_action( 'wp_ajax_nopriv_phone_link', 'tsml_ajax_phone_link' );
  *
  */
 function tsml_ajax_phone_link() {
+
 	global $tsml_nonce;
 
 	if ( ! isset( $_GET['meeting_id'] ) || ! isset( $_GET['nonce'] ) || ! wp_verify_nonce( $_GET['nonce'], $tsml_nonce ) ) {
 		wp_send_json_error();
 	}
 	$meeting_id = intval( $_GET['meeting_id'] );
-	$phone = get_post_meta( $meeting_id, 'conference_phone', true );
+	$phone      = get_post_meta( $meeting_id, 'conference_phone', true );
 	if ( $phone ) {
 		wp_send_json_success( ['phone' => 'tel:' . $phone]);
 	} else {
