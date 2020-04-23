c1f2f85a1ccee9f923e8f355ecda7e5a82d4fae2
Mark Chouinard <mark@chouinard.me>
2020-04-23 13:42:11 -0700

    Check $post_type is set.
    Signed-off-by: Mark Chouinard <mark@chouinard.me>

--- includes/functions.php
+++ includes/functions.php
@@ -31,7 +31,7 @@ function tsml_assets() {
 	global $post_type, $tsml_street_only, $tsml_programs, $tsml_strings, $tsml_program, $tsml_google_maps_key, $tsml_mapbox_key, $tsml_google_overrides, $tsml_distance_units, $tsml_defaults, $tsml_language, $tsml_columns, $tsml_nonce;
 
 	// TODO: verify this doesn't cause any other issues
-	if ( 'tsml_meeting' !== $post_type ) {
+	if ( isset( $post_type ) && 'tsml_meeting' !== $post_type ) {
 		return;
 	}
 	//google maps api
