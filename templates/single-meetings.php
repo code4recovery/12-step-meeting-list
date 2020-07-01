6748984c649d156510a005468cbea56f200eccbf
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-07-01 08:34:33 -0700

    payment method buttons

--- templates/single-meetings.php
+++ templates/single-meetings.php
@@ -95,10 +95,21 @@ get_header();
 									</h3>
 									<?php
 									if (!empty($meeting->conference_url) && $provider = tsml_conference_provider($meeting->conference_url)) {?>
-										<a id="meeting-link" class="btn btn-default btn-block" href="#"><i class="glyphicon glyphicon-facetime-video"></i> <?php echo $provider === true ? $meeting->conference_url : $provider?></a>
+										<a id="meeting-link" class="btn btn-default btn-block" href="#">
+											<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+												<path fill-rule="evenodd" d="M2.667 3.5c-.645 0-1.167.522-1.167 1.167v6.666c0 .645.522 1.167 1.167 1.167h6.666c.645 0 1.167-.522 1.167-1.167V4.667c0-.645-.522-1.167-1.167-1.167H2.667zM.5 4.667C.5 3.47 1.47 2.5 2.667 2.5h6.666c1.197 0 2.167.97 2.167 2.167v6.666c0 1.197-.97 2.167-2.167 2.167H2.667A2.167 2.167 0 0 1 .5 11.333V4.667z"/>
+												<path fill-rule="evenodd" d="M11.25 5.65l2.768-1.605a.318.318 0 0 1 .482.263v7.384c0 .228-.26.393-.482.264l-2.767-1.605-.502.865 2.767 1.605c.859.498 1.984-.095 1.984-1.129V4.308c0-1.033-1.125-1.626-1.984-1.128L10.75 4.785l.502.865z"/>
+											</svg> 
+											<?php echo $provider === true ? $meeting->conference_url : $provider?>
+										</a>
 									<?php }
 									if (!empty($meeting->conference_phone)) {?>
-										<a id="phone-link" class="btn btn-default btn-block" href="#"><i class="glyphicon glyphicon-earphone"></i> <?php echo "Join by phone"?></a>
+										<a id="phone-link" class="btn btn-default btn-block" href="#">
+											<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+												<path fill-rule="evenodd" d="M3.925 1.745a.636.636 0 0 0-.951-.059l-.97.97c-.453.453-.62 1.095-.421 1.658A16.47 16.47 0 0 0 5.49 10.51a16.471 16.471 0 0 0 6.196 3.907c.563.198 1.205.032 1.658-.421l.97-.97a.636.636 0 0 0-.06-.951l-2.162-1.682a.636.636 0 0 0-.544-.115l-2.052.513a1.636 1.636 0 0 1-1.554-.43L5.64 8.058a1.636 1.636 0 0 1-.43-1.554l.513-2.052a.636.636 0 0 0-.115-.544L3.925 1.745zM2.267.98a1.636 1.636 0 0 1 2.448.153l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
+											</svg>
+											<?php _e('Join by phone', '12-step-meeting-list')?>
+										</a>
 									<?php }?>
 								</li>
 								<?php }?>
@@ -160,21 +171,35 @@ get_header();
 												<a href="tel:<?php echo $meeting->phone ?>"><?php echo $meeting->phone ?></a>
 											</p>
 										<?php }
-										if (!empty($meeting->venmo)) {?>
-											<p class="venmo">
-												Venmo: <a href="https://venmo.com/<?php echo substr($meeting->venmo, 1) ?>" target="_blank"><?php echo $meeting->venmo ?></a>
-											</p>
-										<?php }
-										if (!empty($meeting->square)) {?>
-											<p class="square">
-												Square Cash: <a href="https://cash.app/<?php echo $meeting->square ?>" target="_blank"><?php echo $meeting->square ?></a>
-											</p>
-										<?php }
-										if (!empty($meeting->paypal)) {?>
-											<p class="paypal">
-												PayPal: <a href="https://www.paypal.me/<?php echo $meeting->paypal ?>" target="_blank"><?php echo $meeting->paypal ?></a>
-											</p>
-										<?php }
+										$services = array(
+											'venmo' => array(
+												'name' => 'Venmo',
+												'url' => 'https://venmo.com/',
+												'substr' => 1,
+											), 
+											'square' => array(
+												'name' => 'Cash App',
+												'url' => 'https://cash.app/',
+												'substr' => 0,
+											),
+											'paypal' => array(
+												'name' => 'PayPal',
+												'url' => 'https://www.paypal.me/',
+												'substr' => 0,
+											)
+											);
+										foreach ($services as $field => $service) {
+											if (!empty($meeting->{$field})) {?>
+												<a id="paypal-link" class="btn btn-default btn-block" href="<?php echo $service['url'] . substr($meeting->{$field}, $service['substr']) ?>" target="_blank">
+													<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+														<path d="M14 3H1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1h-1z"/>
+														<path fill-rule="evenodd" d="M15 5H1v8h14V5zM1 4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H1z"/>
+														<path d="M13 5a2 2 0 0 0 2 2V5h-2zM3 5a2 2 0 0 1-2 2V5h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 13a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+													</svg>
+													<?php echo sprintf(__('Contribute with %s', '12-step-meeting-list'), $service['name'])?>
+												</a>
+											<?php }
+										}
 										?>
 									</li>
 								<?php }?>
