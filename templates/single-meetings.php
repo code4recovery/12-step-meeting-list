cce78bcc05559c1b4e64158714dee14144979fc6
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-07-04 10:42:14 -0700

    overhauling display of individual group contacts

--- templates/single-meetings.php
+++ templates/single-meetings.php
@@ -112,10 +112,47 @@ get_header();
 										</a>
 									<?php }?>
 								</li>
-								<?php }?>
+								<?php }
 
-								</li>
-								<?php
+								$services = array(
+									'venmo' => array(
+										'name' => 'Venmo',
+										'url' => 'https://venmo.com/',
+										'substr' => 1,
+									), 
+									'square' => array(
+										'name' => 'Cash App',
+										'url' => 'https://cash.app/',
+										'substr' => 0,
+									),
+									'paypal' => array(
+										'name' => 'PayPal',
+										'url' => 'https://www.paypal.me/',
+										'substr' => 0,
+									)
+								);
+								$active_services = array_filter(array_keys($services), function($service) use ($meeting) { return !empty($meeting->{$service}); });
+								if (count($active_services)) {?>
+									<li class="list-group-item list-group-item-group">
+										<h3 class="list-group-item-heading"><?php _e('7th Tradition', '12-step-meeting-list')?></h3>
+										<?php
+										foreach ($active_services as $field) {
+											$service = $services[$field];
+											if (!empty($meeting->{$field})) {?>
+												<a id="paypal-link" class="btn btn-default btn-block" href="<?php echo $service['url'] . substr($meeting->{$field}, $service['substr']) ?>" target="_blank">
+													<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+														<path d="M14 3H1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1h-1z"/>
+														<path fill-rule="evenodd" d="M15 5H1v8h14V5zM1 4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H1z"/>
+														<path d="M13 5a2 2 0 0 0 2 2V5h-2zM3 5a2 2 0 0 1-2 2V5h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 13a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+													</svg>
+													<?php echo sprintf(__('Contribute with %s', '12-step-meeting-list'), $service['name'])?>
+												</a>
+											<?php }
+										}
+										?>
+									</li>
+								<?php }
+
 								if (!empty($meeting->location_id)) {
 									$location_info = '
 										<h3 class="list-group-item-heading">' . $meeting->location . '</h3>';
@@ -142,11 +179,10 @@ get_header();
 									);
 								}
 
-								if (!empty($meeting->group) || !empty($meeting->website) || !empty($meeting->website_2) || !empty($meeting->email) || !empty($meeting->phone) || !empty($meeting->venmo) || !empty($meeting->square) || !empty($meeting->paypal)) {?>
+								if (!empty($meeting->group) || !empty($meeting->website) || !empty($meeting->website_2) || !empty($meeting->email) || !empty($meeting->phone || ($tsml_contact_display == 'public'))) {?>
 									<li class="list-group-item list-group-item-group">
-										<?php if (!empty($meeting->group)) {?>
-											<h3 class="list-group-item-heading"><?php echo $meeting->group ?></h3>
-										<?php }
+										<h3 class="list-group-item-heading"><?php echo empty($meeting->group) ? __('Contact Information', '12-step-meeting-list') : $meeting->group ?></h3>
+										<?php
 										if (!empty($meeting->group_notes)) {?>
 											<section class="group-notes"><?php echo wpautop($meeting->group_notes) ?></section>
 										<?php }
@@ -191,38 +227,31 @@ get_header();
 												<?php _e('Group Phone', '12-step-meeting-list')?>
 											</a>
 										<?php }
-										$services = array(
-											'venmo' => array(
-												'name' => 'Venmo',
-												'url' => 'https://venmo.com/',
-												'substr' => 1,
-											), 
-											'square' => array(
-												'name' => 'Cash App',
-												'url' => 'https://cash.app/',
-												'substr' => 0,
-											),
-											'paypal' => array(
-												'name' => 'PayPal',
-												'url' => 'https://www.paypal.me/',
-												'substr' => 0,
-											)
-											);
-										foreach ($services as $field => $service) {
-											if (!empty($meeting->{$field})) {?>
-												<a id="paypal-link" class="btn btn-default btn-block" href="<?php echo $service['url'] . substr($meeting->{$field}, $service['substr']) ?>" target="_blank">
-													<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-														<path d="M14 3H1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1h-1z"/>
-														<path fill-rule="evenodd" d="M15 5H1v8h14V5zM1 4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H1z"/>
-														<path d="M13 5a2 2 0 0 0 2 2V5h-2zM3 5a2 2 0 0 1-2 2V5h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 13a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-													</svg>
-													<?php echo sprintf(__('Contribute with %s', '12-step-meeting-list'), $service['name'])?>
-												</a>
-											<?php }
+										if ($tsml_contact_display == 'public') {
+											for ($i = 1; $i <= GROUP_CONTACT_COUNT; $i++) {
+												$name = empty($meeting->{'contact_' . $i . '_name'}) ? sprintf(__('Contact %n', '12-step-meeting-list'), $i) : $meeting->{'contact_' . $i . '_name'};
+												if (!empty($meeting->{'contact_' . $i . '_email'})) {?>
+													<a href="mailto:<?php echo $meeting->{'contact_' . $i . '_email'} ?>" class="btn btn-default btn-block contact-email">
+														<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+															<path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+														</svg>
+														<?php echo sprintf(__('%s Email', '12-step-meeting-list'), $name)?>
+													</a>													
+												<?php }
+												if (!empty($meeting->{'contact_' . $i . '_phone'})) {?>
+													<a href="tel:<?php echo $meeting->{'contact_' . $i . '_phone'} ?>" class="btn btn-default btn-block contact-phone">
+														<svg width="1em" height="1em" viewBox="0 0 16 16" style="margin: 0 3px -2px 0" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+															<path fill-rule="evenodd" d="M3.925 1.745a.636.636 0 0 0-.951-.059l-.97.97c-.453.453-.62 1.095-.421 1.658A16.47 16.47 0 0 0 5.49 10.51a16.471 16.471 0 0 0 6.196 3.907c.563.198 1.205.032 1.658-.421l.97-.97a.636.636 0 0 0-.06-.951l-2.162-1.682a.636.636 0 0 0-.544-.115l-2.052.513a1.636 1.636 0 0 1-1.554-.43L5.64 8.058a1.636 1.636 0 0 1-.43-1.554l.513-2.052a.636.636 0 0 0-.115-.544L3.925 1.745zM2.267.98a1.636 1.636 0 0 1 2.448.153l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
+														</svg>
+														<?php echo sprintf(__('%s Phone', '12-step-meeting-list'), $name)?>
+													</a>													
+												<?php }
+											}
 										}
 										?>
 									</li>
-								<?php }?>
+									<?php
+								}?>
 								<li class="list-group-item list-group-item-updated">
 									<?php _e('Updated', '12-step-meeting-list')?>
 									<?php the_modified_date()?>
@@ -231,26 +260,6 @@ get_header();
 						</div>
 
 						<?php
-						if ($tsml_contact_display == 'public') {
-							for ($i = 1; $i <= GROUP_CONTACT_COUNT; $i++) {
-								if (!empty($meeting->{'contact_' . $i . '_name'}) || !empty($meeting->{'contact_' . $i . '_email'}) || !empty($meeting->{'contact_' . $i . '_phone'})) {?>
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h3 class="panel-title">
-											<?php if (!empty($meeting->{'contact_' . $i . '_name'})) {
-												echo $meeting->{'contact_' . $i . '_name'};
-											}?>
-											<span class="panel-title-buttons">
-												<?php if (!empty($meeting->{'contact_' . $i . '_email'})) {?><a href="mailto:<?php echo $meeting->{'contact_' . $i . '_email'} ?>"><span class="glyphicon glyphicon-envelope"></span></a><?php }?>
-												<?php if (!empty($meeting->{'contact_' . $i . '_phone'})) {?><a href="tel:<?php echo preg_replace('~\D~', '', $meeting->{'contact_' . $i . '_phone'}) ?>"><span class="glyphicon glyphicon-earphone"></span></a><?php }?>
-											</span>
-										</h3>
-									</div>
-								</div>
-								<?php }
-							}
-						}
-
 						if (!empty($tsml_feedback_addresses)) {?>
 						<form id="feedback">
 							<input type="hidden" name="action" value="tsml_feedback">
