8b16ffd44bdb97a6e94bd7fc75488b82b51c3ffb
Brian <brianw.area24@gmail.com>
2020-07-19 23:42:37 -0500

    Change where tsml_location status set to 'publish'

--- includes/save.php
+++ includes/save.php
@@ -27,12 +27,6 @@ function tsml_save_post($post_id, $post, $update) {
 	//update is always 1, probably because it's actually 'created' when the edit screen first loads (due to autosave)
 	$update = ($post->post_date !== $post->post_modified);
 
-	// Set the meeting's location status to 'publish'
-	// tsml_delete_orphans() will undo this later if necessary
-	if (!empty($post->post_parent)) {
-		wp_update_post(array('ID' => $post->post_parent, 'post_status' => 'publish'));
-	}
-	
 	//sanitize strings (website, website_2, paypal are not included)
 	$strings = array('post_title', 'location', 'formatted_address', 'mailing_address', 'venmo', 'square', 'post_status', 'group', 'last_contact', 'conference_phone');
 	foreach ($strings as $string) {
@@ -200,7 +194,13 @@ function tsml_save_post($post_id, $post, $update) {
 					'post_content'  => $_POST['location_notes'],
 				));
 			}
-	
+
+			// Set the meeting's location status to 'publish'
+			// tsml_delete_orphans() will change it back to draft if necessary
+			if ($locations[0]->post_status != 'publish') {
+				wp_update_post(array('ID' => $location_id, 'post_status' => 'publish'));
+			}
+
 			//latitude longitude only if updated
  			foreach (array('latitude', 'longitude') as $field) {
 				if (!$update || $old_meeting->{$field} != $_POST[$field]) {
