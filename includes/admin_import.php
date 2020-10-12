094e4bf32e890482ceb8f725c5baf8f73b777a63
Mark Brown <mark@97eighty.com>
2020-10-11 18:42:20 -0700

    Change URL Issue 176

--- includes/admin_import.php
+++ includes/admin_import.php
@@ -636,7 +636,7 @@ function tmsl_import_page() {
 							<h3><?php _e('Need Help?', '12-step-meeting-list')?></h3>
 							<p><?php _e('This plugin is maintained by a group of AA volunteers. To get help, please use the support threads on WordPress.org.', '12-step-meeting-list')?></p>
 							<p>
-								<a href="https://wordpress.org/support/plugin/12-step-meeting-list/" target="_blank" class="button">
+								<a href="https://github.com/code4recovery/12-step-meeting-list/issues" target="_blank" class="button">
 									<?php _e('Get Support', '12-step-meeting-list')?>
 								</a>
 							</p>
