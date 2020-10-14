62a80fd8df616c28d758c43255f7c9ebedb391a5
Tech2Serve <tech2serve@code4recovery.org>
2020-10-13 23:30:51 -0500

    feat: Disable map markers, info boxes and directions for approximate locations
    - Improves use of city and state only for online meetings
    - Changes implemented for single meeting, single location and admin pages
    - Tested with Google and Mapbox
    - Additional `tsml.debug` console.log statements provided in various places.
    
    TODO:
    -[ ] Code doesn't test to confirm online meeting information has been set. This should
    happen by setting meeting as 'online' (see defined $tsml_meeting_attendance_options)
    
    see #173

--- templates/single-locations.php
+++ templates/single-locations.php
@@ -6,6 +6,7 @@ $location = tsml_get_location();
 //define some vars for the map
 wp_localize_script('tsml_public', 'tsml_map', array(
 	'formatted_address' => $location->formatted_address,
+	'is_approximate_location' => $location->is_approximate_location,
 	'directions' => __('Directions', '12-step-meeting-list'),
 	'directions_url' => $location->directions,
 	'latitude' => $location->latitude,
@@ -38,6 +39,7 @@ get_header();
 
 				<div class="row location">
 					<div class="col-md-4">
+					<?php if (!($location->is_approximate_location)) { ?>
 						<div class="panel panel-default">
 							<a class="panel-heading tsml-directions" data-latitude="<?php echo $location->latitude ?>" data-longitude="<?php echo $location->longitude ?>" data-location="<?php echo $location->post_title ?>">
 								<h3 class="panel-title">
@@ -48,6 +50,7 @@ get_header();
 								</h3>
 							</a>
 						</div>
+						<?php }?>
 
 						<div class="panel panel-default">
 							<ul class="list-group">
