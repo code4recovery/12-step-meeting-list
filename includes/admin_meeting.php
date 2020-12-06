1e5d231261534c498db1705d4f42435c2c78374d
Tech2Serve <tech2serve@code4recovery.org>
2020-12-05 21:03:30 -0600

    fix: (wip) Remove is_approximate_location refs from database
    To prepare for the new implementation fix that improves how we handle
    approximate locations.
    
    see #223

--- includes/admin_meeting.php
+++ includes/admin_meeting.php
@@ -30,8 +30,10 @@ function tsml_admin_init() {
 	// Compares versions and updates databases as needed for upgrades
 	$tsml_version = get_option('tsml_version');
 	if (version_compare($tsml_version, TSML_VERSION, '<=')) {
-		db_update_addresses_cache_approximate_location();
-		db_update_tsml_locations_approximate_location();
+    db_update_remove_all_approximate_location_cache();
+    db_update_remove_all_is_approximate_location_meta();
+		// db_update_addresses_cache_approximate_location();
+		// db_update_tsml_locations_approximate_location();
 		update_option('tsml_version', TSML_VERSION);
 		flush_rewrite_rules();
 	};
--- includes/database_updates.php
+++ includes/database_updates.php
@@ -37,3 +37,34 @@ if (!function_exists('db_update_tsml_locations_approximate_location')) {
     }
   };
 };
+
+// Function: db_update_remove_all_approximate_location_cache
+if (!function_exists('db_update_remove_all_approximate_location_cache')) {
+	function db_update_remove_all_approximate_location_cache() {
+    $addresses_cache = get_option('tsml_addresses');
+    $updated_address_cache = array();
+		if (!empty($addresses_cache)) {
+			foreach ( $addresses_cache as $key => $entry ) {
+        $address = $entry['formatted_address'];
+				if (array_key_exists('is_approximate_location', $entry)) {
+          unset($entry['is_approximate_location']);
+        };
+        $updated_address_cache[$address] = $entry;
+      };
+      update_option('tsml_addresses', $updated_address_cache);
+    };
+  };
+};
+
+// Function: db_update_remove_all_is_approximate_location_meta
+if (!function_exists('db_update_remove_all_is_approximate_location_meta')) {
+  function db_update_remove_all_is_approximate_location_meta() {
+    $location_posts = tsml_get_all_locations();
+    foreach ($location_posts as $location_post) {
+      $location_custom = get_post_meta($location_post->ID);
+      if (array_key_exists('is_approximate_location', $location_custom)) {
+        delete_post_meta($location_post->ID, 'is_approximate_location');
+      };
+    };
+  };
+};
