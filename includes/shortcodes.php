a39102c4b5a5086ce90ea916023397eba25bd2af
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2021-05-30 17:55:52 -0700

    more configuration options for tsml_ui shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -85,12 +85,23 @@ add_shortcode('tsml_types_list', 'tsml_types_list');
 //output a react meeting finder widget https://github.com/code4recovery/tsml-ui
 if (!function_exists('tsml_ui')) {
 	function tsml_ui() {
-		global $tsml_mapbox_key, $tsml_nonce, $tsml_sharing, $tsml_conference_providers;
+		global $tsml_mapbox_key, $tsml_nonce, $tsml_sharing, $tsml_conference_providers, $tsml_language, $tsml_columns, $tsml_programs, $tsml_program, $tsml_ui_config;
 		$js = defined('TSML_UI_PATH') ? TSML_UI_PATH : 'https://cdn.jsdelivr.net/gh/code4recovery/tsml-ui/public/app.js';
 		wp_enqueue_script('tsml_ui', $js);
-		wp_localize_script('tsml_ui', 'tsml_react_config', array(
-			'timezone' => get_option('timezone_string'),
-			'conference_providers' => $tsml_conference_providers
+		wp_localize_script('tsml_ui', 'tsml_react_config', array_merge(
+			array(
+				'timezone' => get_option('timezone_string', 'America/New_York'),
+				'conference_providers' => $tsml_conference_providers,
+				'strings' => array(
+					$tsml_language => array_merge(
+						$tsml_columns, 
+						array(
+							'types' => $tsml_programs[$tsml_program]['types'],
+						),
+					),
+				),
+			),
+			$tsml_ui_config,
 		));
 		$data = admin_url('admin-ajax.php') . '?action=meetings&nonce=' . wp_create_nonce($tsml_nonce);
 		return '<meetings src="' . $data . '" mapbox="' . $tsml_mapbox_key . '"/>';
