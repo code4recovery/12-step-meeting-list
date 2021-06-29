2f99e87f51fcede93bb0d7490b949cd9e77464b8
Brian Webster <bwebster719@gmail.com>
2021-06-28 21:46:39 -0500

    Removed unused function: db_update_addresses_cache_approximate_location

--- includes/database_updates.php
+++ includes/database_updates.php
@@ -9,24 +9,6 @@ if(!function_exists('decide_if_location_approximate')) {
   }
 }
 
-// Function: db_update_addresses_cache_approximate_location
-if (!function_exists('db_update_addresses_cache_approximate_location')) {
-	function db_update_addresses_cache_approximate_location() {
-    $addresses_cache = get_option('tsml_addresses');
-    $updated_address_cache = array();
-		if (!empty($addresses_cache)) {
-			foreach ( $addresses_cache as $key => $entry ) {
-        $address = $entry['formatted_address'];
-				if (!array_key_exists('is_approximate_location', $entry)) {
-          $entry['is_approximate_location'] = decide_if_location_approximate($address);					
-        };
-        $updated_address_cache[$key] = $entry;
-      };
-      update_option('tsml_addresses', $updated_address_cache);
-    };
-  };
-};
-
 if (!function_exists('db_update_tsml_locations_approximate_location')) {
   function db_update_tsml_locations_approximate_location() {
     $locations_posts = tsml_get_all_locations();
