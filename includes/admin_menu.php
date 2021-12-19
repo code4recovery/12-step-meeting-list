357aa8ecf7a50c701f539c55f216f89c3ef06556
Brian W <brianw.area24@gmail.com>
2021-12-18 19:38:22 -0600

    Code to include a widget on the main WordPress Dashboard

--- includes/admin_menu.php
+++ includes/admin_menu.php
@@ -39,3 +39,17 @@ add_action('admin_menu', function () {
     }
     add_filter('parent_file', 'tsml_fix_highlight');
 });
+
+// Add a Widget to the main WordPress Dashboard Page
+add_action('wp_dashboard_setup', 'tsml_dashboard_widgets');
+function tsml_dashboard_widgets()
+{
+    global $wp_meta_boxes;
+
+    wp_add_dashboard_widget('tsml_help_widget', 'Code for Recovery/12 Step Meeting List(TSML) Plugin', 'tsml_dashboard_help');
+}
+function tsml_dashboard_help()
+{
+    echo '<p><a href="https://code4recovery.org/">Code for Recovery</a>, the group that maintains TSML, and other recovery based projects, has a <a href="https://code4recovery.org/">new web site</a>. We continue to add information to this site, including information about our group, our projects, our volunteer opportunities. We are also adding project documentation to the website.</p>';
+    echo '<p>Code for Recovery now has 501c3 nonprofit status, so we can take contribuitions to cover costs and build up a prudent reserve. More information at our <a href="https://code4recovery.org/donate">Donate to Code for Recovery</a> page.</p>';
+}
