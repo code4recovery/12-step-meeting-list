73acff633303fa1ca64663f2bd04deabfaa4efb1
kiyote33 <wayne@parkercanada.net>
2022-01-01 13:01:48 -0700

    Allow theme-defined function to reformat  data source import - issue #439

--- includes/functions.php
+++ includes/functions.php
@@ -1288,6 +1288,10 @@ function tsml_import_buffer_set($meetings, $data_source = null)
 		$meetings = tsml_import_reformat_googlesheet($meetings);
 	}
 
+	if (function_exists('tsml_import_reformat')) { 
+		$meetings = tsml_import_reformat($meetings);
+	}
+
 	//uppercasing for value matching later
 	$upper_types = array_map('strtoupper', $tsml_programs[$tsml_program]['types']);
 	$upper_days = array_map('strtoupper', $tsml_days);
