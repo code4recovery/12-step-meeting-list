35879c66e1d4c33ff8e8010b027e2d16d8d98ab1
Brian W <brianw.area24@gmail.com>
2022-03-06 00:30:04 -0600

    Changed the way attendance options is displayed

--- templates/single-meetings.php
+++ templates/single-meetings.php
@@ -93,22 +93,11 @@ get_header();
 										$li_marker = '<svg class="icon" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 													<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
 												</svg>';
-										switch ($meeting->attendance_option) {
-											case 'in_person':
-												echo '<li>' . $li_marker . __('In person', '12-step-meeting-list') . '</li>' . PHP_EOL;
-												break;
-											case 'hybrid':
-												echo '<li>' . $li_marker . __('In person', '12-step-meeting-list') . '</li>' . PHP_EOL;
-												echo '<li>' . $li_marker . __('Online', '12-step-meeting-list') . '</li>' . PHP_EOL;
-												break;
-											case 'online':
-												echo '<li>' . $li_marker . __('Online', '12-step-meeting-list') . '</li>' . PHP_EOL;
-												break;
-											case 'inactive':
-												echo '<li>' . $li_marker . __('Inactive', '12-step-meeting-list') . '</li>' . PHP_EOL;
-												break;
-											default:
-												break;
+										if ($meeting->attendance_option === 'hybrid') {
+											echo '<li>' . $li_marker . __('In-person', '12-step-meeting-list') . '</li>' . PHP_EOL;
+											echo '<li>' . $li_marker . __('Online', '12-step-meeting-list') . '</li>' . PHP_EOL;
+										} else {
+											echo '<li>' . $li_marker . $tsml_meeting_attendance_options[$meeting->attendance_option] . '</li>' . PHP_EOL;
 										}
 										?>
 										<li>
