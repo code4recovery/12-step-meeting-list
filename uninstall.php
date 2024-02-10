f79f3f8431a106facb995410f2c983570b1e2b74
George Kovats <kovatsgeorge@gmail.com>
2024-02-10 15:30:57 -0500

    `tsml_delete` called without bootstrap Uninstall step apparently runs without loading the plugin first. So, requiring up front.

--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
+require_once trailingslashit(__DIR__) . '12-step-meeting-list.php';
+
 tsml_delete('everything');
 
 //flush rewrite once more for good measure
