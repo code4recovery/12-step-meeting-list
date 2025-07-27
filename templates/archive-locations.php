faf8ab0cb29127dd7ecb705b4377b6cfee120d3e
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2025-07-26 20:53:21 -0700

    adding schema markup

--- templates/archive-locations.php
+++ templates/archive-locations.php
@@ -1,47 +1,106 @@
 <?php
-get_header();
+
+$meetings = tsml_get_meetings();
+
+$title = __(sprintf('Index of %s Meetings', $tsml_programs[$tsml_program]['name']), '12-step-meeting-list');
+
+$days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+$website_timezone = get_option('timezone_string');
+
+$schema = [
+    '@context' => 'https://schema.org',
+    '@graph' => array_map(function ($meeting) use ($days, $website_timezone) {
+        $meeting['time'] = DateTime::createFromFormat('H:i', $meeting['time']);
+        if (empty($meeting['end_time'])) {
+            $meeting['end_time'] = $meeting['time']->modify('+1 hour');
+        } else {
+            $meeting['end_time'] = DateTime::createFromFormat('H:i', $meeting['end_time']);
+        }
+        $timezone = $meeting['timezone'] ?? $website_timezone;
+
+        return [
+            '@type' => 'Event',
+            'name' => $meeting['name'],
+            'url' => @$meeting['url'],
+            'description' => tsml_meeting_types($meeting['types']),
+            "eventSchedule" => [
+                "@type" => "Schedule",
+                "repeatFrequency" => "P1W",
+                "byDay" => "https://schema.org/" . $days[$meeting['day']],
+                "startTime" => $meeting['time']->format('H:i'),
+                "endTime" => $meeting['end_time']->format('H:i'),
+                "scheduleTimezone" => $timezone
+            ],
+            'location' => [
+                '@type' => 'Place',
+                'name' => @$meeting['location'],
+                'address' => @$meeting['formatted_address'],
+                'geo' => [
+                    '@type' => $meeting['approximate'] ? 'GeoCircle' : 'GeoCoordinates',
+                    'latitude' => @$meeting['latitude'],
+                    'longitude' => @$meeting['longitude']
+                ]
+            ]
+        ];
+    }, array_filter($meetings, function ($meeting) {
+        return !empty($meeting['name']) && !empty($meeting['day']) && !empty($meeting['time']);
+    })),
+];
 ?>
+<!doctype html>
+<html lang="<?php echo substr(get_locale(), 0, 2); ?>">
 
-<h1>Index of <?php echo $tsml_programs[$tsml_program]['name']; ?> Meetings</h1>
+<head>
+    <title><?php echo esc_html($title); ?></title>
+    <script type="application/ld+json">
+        <?php echo json_encode($schema); ?>
+    </script>
+</head>
 
-<p>
-    This page is intended for web crawlers. To find a meeting, please visit our
-    <a href="<?php echo esc_url(get_post_type_archive_link('tsml_meeting')); ?>">meetings page</a>.
-</p>
+<body>
 
-<table border="1" cellpadding="5" cellspacing="0">
-    <thead>
-        <tr>
-            <th>Day</th>
-            <th>Time</th>
-            <th>Meeting</th>
-            <th>Location</th>
-            <th>Formatted Address</th>
-            <th>Region</th>
-            <th>Sub-Region</th>
-            <th>Types</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $meetings = tsml_get_meetings();
-        foreach ($meetings as $meeting) { ?>
+    <h1><?php echo esc_html($title); ?></h1>
+
+    <p>
+        <?php _e(sprintf(
+            'This page is intended for web crawlers. To find a meeting, please visit our <a href="%s">meetings page</a>.',
+            esc_url(get_post_type_archive_link('tsml_meeting'))
+        ), '12-step-meeting-list'); ?>
+    </p>
+
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
             <tr>
-                <td><?php echo esc_html(@$tsml_days[$meeting['day']]); ?></td>
-                <td><?php echo tsml_format_time(@$meeting['time']); ?></td>
-                <td><a href="<?php echo esc_url($meeting['url']); ?>"><?php echo esc_html($meeting['name']); ?></a></td>
-                <td><?php echo esc_html($meeting['location'] ?? ''); ?></td>
-                <td><?php echo esc_html($meeting['formatted_address'] ?? ''); ?></td>
-                <td><?php echo esc_html($meeting['region'] ?? ''); ?></td>
-                <td><?php echo esc_html($meeting['sub_region'] ?? ''); ?></td>
-                <td><?php echo esc_html(tsml_meeting_types($meeting['types']) ?? ''); ?></td>
-                <td><?php echo esc_html($meeting['latitude'] ?? ''); ?></td>
-                <td><?php echo esc_html($meeting['longitude'] ?? ''); ?></td>
+                <th>Day</th>
+                <th>Time</th>
+                <th>Meeting</th>
+                <th>Location</th>
+                <th>Formatted Address</th>
+                <th>Region</th>
+                <th>Sub-Region</th>
+                <th>Types</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
             </tr>
-        <?php } ?>
-    </tbody>
-</table>
-<?php
-get_footer();
+        </thead>
+        <tbody>
+            <?php foreach ($meetings as $meeting) { ?>
+                <tr>
+                    <td><?php echo esc_html(@$tsml_days[$meeting['day']]); ?></td>
+                    <td><?php echo tsml_format_time(@$meeting['time']); ?></td>
+                    <td><a href="<?php echo esc_url($meeting['url']); ?>"><?php echo esc_html($meeting['name']); ?></a></td>
+                    <td><?php echo esc_html($meeting['location'] ?? ''); ?></td>
+                    <td><?php echo esc_html($meeting['formatted_address'] ?? ''); ?></td>
+                    <td><?php echo esc_html($meeting['region'] ?? ''); ?></td>
+                    <td><?php echo esc_html($meeting['sub_region'] ?? ''); ?></td>
+                    <td><?php echo esc_html(tsml_meeting_types($meeting['types']) ?? ''); ?></td>
+                    <td><?php echo esc_html($meeting['latitude'] ?? ''); ?></td>
+                    <td><?php echo esc_html($meeting['longitude'] ?? ''); ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
