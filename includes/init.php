485567c2ec9b5572181ea44d3404f660e035a073
George Kovats <kovatsgeorge@gmail.com>
2025-07-30 22:26:53 -0400

    Adding new permalink for feed

--- includes/init.php
+++ includes/init.php
@@ -78,13 +78,38 @@ add_action('init', function () {
         return $template;
     });
 
-
     // add theme name to body class, for per-theme CSS fixes
     add_filter('body_class', function ($classes) {
         $theme = wp_get_theme();
         $classes[] = sanitize_title($theme->Template);
         return $classes;
     });
+
+    add_rewrite_rule( '^tsmlfeed/([A-Za-z0-9]{12,32})/?$', 'index.php?tsml_feed=$matches[1]', 'top' );
+    add_rewrite_rule( '^tsmlfeed/?$', 'index.php?tsml_feed=open', 'top' );
+
+    add_filter( 'query_vars', function( $query_vars ) {
+        $query_vars[] = 'tsml_feed';
+        return $query_vars;
+    } );
+    
+    add_filter( 'template_include', function( $template ) {
+        global $tsml_sharing, $tsml_sharing_keys;
+
+        $tsml_feed = get_query_var( 'tsml_feed' );
+
+        if ($tsml_feed) {
+            if ('open' === $tsml_sharing || array_key_exists($tsml_feed, $tsml_sharing_keys)) {
+                if (!headers_sent()) {
+                    header('Access-Control-Allow-Origin: *');
+                }
+                wp_send_json(tsml_get_meetings(empty($_POST) ? $_GET : $_POST));
+                exit;
+            }
+        }
+
+        return $template;
+    } );
 
 });
 
